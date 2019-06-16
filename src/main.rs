63099e359686fc60a25aa960071c0c2c014aaf08
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2019-06-16 18:57:33 +0900

    Add a macro `print_ode_ts`
    This macro generates a code to drive the ODE solver and print out
    the time series.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@ use soo::SystemFromConfig;
 use eom::traits::Scheme;
 use eom::ode::roessler::Roessler;
 
+macro_rules! print_ode_ts {
+    ($solver:ty, $ode:expr, $init:expr, $step_size:expr, $range_start:expr, $range_end:expr) => {
+        let mut t = 0;
+        let mut teo = <$solver>::new($ode, $step_size);
+        let mut ts = eom::adaptor::time_series(ndarray::arr1($init), &mut teo);
+        loop {
+            if let Some(end) = $range_end {
+                if t > end { break; }
+            }
+            let v = ts.next().unwrap();
+            if t >= $range_start {
+                let time = t as f64 * $step_size;
+                println!("{:.6} {:.6} {:.6} {:.6}", time, v[0], v[1], v[2]);
+            }
+            t += 1;
+        }
+    };
+}
+
 fn main() {
     let mut handle = std::io::stdin();
     let mut conf = String::new();
@@ -14,16 +33,11 @@ fn main() {
     let ode = Roessler::system_from_config(&conf);
     println!("{:?}", ode);
 
-    let mut t = 0;
-    let mut teo = eom::explicit::RK4::new(ode, conf.solver.step_size.unwrap());
-    let mut ts = eom::adaptor::time_series(ndarray::arr1(&conf.solver.init.unwrap()), &mut teo);
-    loop {
-        if t > conf.solver.step_range.1.unwrap() {
-            break;
-        }
-        let v = ts.next().unwrap();
-        let time = t as f64 * conf.solver.step_size.unwrap();
-        println!("{:.6} {:.6} {:.6} {:.6}", time, v[0], v[1], v[2]);
-        t += 1;
-    }
+    print_ode_ts!(
+        eom::explicit::RK4<Roessler>,
+        ode,
+        &conf.solver.init.unwrap(),
+        conf.solver.step_size.unwrap(),
+        conf.solver.step_range.0,
+        conf.solver.step_range.1);
 }
