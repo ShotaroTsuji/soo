6becbf8c8c494ed357cd1102a831f182556778c9
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2019-07-29 16:30:21 +0900

    Add harmonic oscillator

---
+++ src/system/harmonic.rs
@@ -0,0 +1,54 @@
+use ndarray::*;
+use eom::traits::*;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Harmonic {
+    pub omega: f64,
+}
+
+impl Default for Harmonic {
+    fn default() -> Self {
+        Harmonic {
+            omega: 1.0
+        }
+    }
+}
+
+impl ModelSpec for Harmonic {
+    type Scalar = f64;
+    type Dim = Ix1;
+
+    fn model_size(&self) -> usize {
+        2
+    }
+}
+
+impl Explicit for Harmonic {
+    fn rhs<'a, S>(&mut self, v: &'a mut ArrayBase<S, Ix1>) -> &'a mut ArrayBase<S, Ix1>
+    where
+        S: DataMut<Elem = f64>,
+    {
+        let x = v[0];
+        let y = v[1];
+
+        v[0] = self.omega * y;
+        v[1] = - self.omega * x;
+
+        v
+    }
+}
+
+use crate::SystemFromConfig;
+use crate::config;
+
+impl SystemFromConfig for Harmonic {
+    fn system_from_config(conf: &config::Config) -> Self {
+        assert_eq!(&conf.system.name, "harmonic");
+
+        let omega = crate::lookup_parameter!("omega", &conf);
+
+        Harmonic {
+            omega: omega,
+        }
+    }
+}
